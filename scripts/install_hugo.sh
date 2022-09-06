013a3c38e844241d9528740b6c0a252a900e3438
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2022-09-06 12:51:11 +0200

    fixes for the install script

--- scripts/install_hugo.sh
+++ scripts/install_hugo.sh
@@ -11,15 +11,19 @@ fi
 
 OS=$(uname)
 if [ "${OS}" == "Darwin" ]; then
-  curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_0.102.3_macOS-universal.tar.gz
+  curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_extended_0.102.3_macOS-universal.tar.gz
 elif [ "${OS}" == "Linux" ]; then
   if [ "$(uname -m)" == "x86_64" ]; then
-    curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_0.102.3_Linux-64bit.tar.gz
+    curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_extended_0.102.3_Linux-64bit.tar.gz
+  elif [ "$(uname -m)" == "arm64" ]; then
+    curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_extended_0.102.3_Linux-ARM64.tar.gz
   else
-    curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.102.3/hugo_0.102.3_Linux-ARM64.tar.gz
+    echo "${OS} $(uname -m) is not supported. go away"
+    exit 1
   fi
 else
   echo "${OS} is not supported. go away"
+  exit 1
 fi
 
 tar xvf hugo.tar.gz hugo
